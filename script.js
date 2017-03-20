d67a4a258e9c1bab90d28b54b96b3ab3b5b7e140
Liam Öberg <Liam@LiamsMacbook.local>
2017-03-20 17:10:48 +0100

    added some navbar boostrap shiet

---
+++ script.js
@@ -0,0 +1,21 @@
+// const footballDataBase = {
+//     getCompetitions: function(){
+//         $.ajax({
+//         method:'GET',
+//         url: 'http://api.football-data.org/v1/competitions/426/leagueTable',
+//         headers: {
+//             'X-Auth-Token': 'bc1c9459624a4143a7d7e78e5e00e85b'
+//         },
+//         success: (response) => {
+//         data = response;
+//             for(let i = 0; i < response.standing.length; i++){
+//             console.log(response.standing[i].teamName + ' ' + response.standing[i].points);
+//         }
+//         },
+//     });
+//     }
+
+// };
+
+// footballDataBase.getCompetitions();
+
