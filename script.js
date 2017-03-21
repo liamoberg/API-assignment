9bd77651e005a2c3dbf457e4d759e5e95e8486cb
Liam Öberg <Liam@LiamsMacbook.local>
2017-03-21 13:34:43 +0100

    Added tables and data to the table

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
-// const footballDataBase = {
-//     getCompetitions: function(){
-//         $.ajax({
-//         method:'GET',
-//         url: 'http://api.football-data.org/v1/competitions/426/leagueTable',
-//         headers: {
-//             'X-Auth-Token': 'bc1c9459624a4143a7d7e78e5e00e85b'
-//         },
-//         success: (response) => {
-//         data = response;
-//             for(let i = 0; i < response.standing.length; i++){
-//             console.log(response.standing[i].teamName + ' ' + response.standing[i].points);
-//         }
-//         },
-//     });
-//     }
+const footballDataBase = {
 
-// };
+    getCompetitions: function(){
+        $.ajax({
+        method:'GET',
+        url: 'http://api.football-data.org/v1/competitions/426/leagueTable',
+        headers: {
+            'X-Auth-Token': 'bc1c9459624a4143a7d7e78e5e00e85b'
+        },
+        success: (response) => {
+        data = response;
+        console.log(data);
+        $('.lang').text(data.standing[0]);
+        $.each(data.standing, function(key, value){
+            $('.tablebody').append("<tr><th scope='row'>"+value.position+"</th><td><img src='"+value.crestURI+"'/>"+value.teamName+"</td><td>"+value.points+"</td><td>"+value.playedGames+"</td><td>"+value.wins+"</td><td>"+value.draws+"</td><td>"+value.losses+"</td></tr>");
+        });
+    },
+    error: function(error){
 
-// footballDataBase.getCompetitions();
+    }
+    });
+    }
+
+};
+footballDataBase.getCompetitions();
 
